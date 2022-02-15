b52473e9760df9d4c6d1f97b75b6de8470131eb5
Pooja Madhukar Shirude <91906850+Pooja78shirude@users.noreply.github.com>
2022-02-15 19:51:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  5 13:55:42 2021
+
+@author: Pooja Shirude
+"""
+
+
+from recog_numbers import recog_numbers
+from to_english import to_english
+import os
+from flask import Flask, render_template, request, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/recognize', methods=['GET', 'POST'])
+def recognize():
+    if request.method == 'POST':
+        # Get image
+        image = request.files['image']
+        # Create tmp directory if not existent
+        if not os.path.exists(os.path.join('static', 'tmp')):
+            os.mkdir(os.path.join('static', 'tmp'))
+
+        # Save image
+        image_path = os.path.join('static', 'tmp', image.filename)
+        image.seek(0)
+        try:
+            image.save(image_path)
+        except:
+            return render_template('index.html', res="Please Select an Image")
+        try:
+            response,b = recog_numbers(image)
+            eng=to_english(response)
+        except:
+            return render_template('index.html', res="Please Select a supported file type.")
+        c=55
+  
+        return render_template('recognize.html',c=c, predictions=response,prob=b, eng=eng , image=url_for('static', filename='tmp/' + image.filename))
+    else:
+        return render_template('index.html')
